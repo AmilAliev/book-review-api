bdb4a66404381437e7c1f5be54399ee0530a0716
AmilAliev <amileliyev762@gmail.com>
2025-12-03 17:51:34 +0400

    error

--- src/main/java/com/bookreview/api/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/bookreview/api/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.bookreview.api.exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,11 +12,14 @@ import java.util.Date;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(BookNotFoundException.class)
     public ResponseEntity<ErrorObject> handleBookNotFoundException(BookNotFoundException ex, WebRequest request) {
+        logger.error("BookNotFoundException: {}", ex.getMessage(), ex);
 
         ErrorObject errorObject = new ErrorObject();
-        errorObject.setStatusCode(HttpStatus.NOT_FOUND.value());
+        errorObject.setStatusCode(Integer.valueOf(HttpStatus.NOT_FOUND.value()));
         errorObject.setMessage(ex.getMessage());
         errorObject.setTimestamp(new Date());
 
@@ -23,13 +28,13 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ReviewNotFoundException.class)
     public ResponseEntity<ErrorObject> handleReviewNotFoundException(ReviewNotFoundException ex, WebRequest request) {
+        logger.error("ReviewNotFoundException: {}", ex.getMessage(), ex);
 
         ErrorObject errorObject = new ErrorObject();
-        errorObject.setStatusCode(HttpStatus.NOT_FOUND.value());
+        errorObject.setStatusCode(Integer.valueOf(HttpStatus.NOT_FOUND.value()));
         errorObject.setMessage(ex.getMessage());
         errorObject.setTimestamp(new Date());
 
         return new ResponseEntity<>(errorObject, HttpStatus.NOT_FOUND);
     }
-
 }
